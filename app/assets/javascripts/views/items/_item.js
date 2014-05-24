7b54835c172b22ff0c2fa23e020d107f691cf941
Frank Kotsianas <frankkotsianas@gmail.com>
2014-05-23 18:04:44 -0700

    Format middle #listPane

--- app/assets/javascripts/views/items/_item.js
+++ app/assets/javascripts/views/items/_item.js
@@ -2,6 +2,8 @@ Asana.Views._Item = Backbone.View.extend({
   template: JST['items/_item'],
   events: {},
 
+  tagName: 'tr',
+  className: 'list-item',
   render: function () {
     var renderedContent = this.template({ item: this.model });
     this.$el.html(renderedContent);
