093d8b419e533f078cb140b08c4fa2ab969e99a6
Frank Kotsianas <frankkotsianas@gmail.com>
2014-05-26 21:15:00 -0700

    Implement EditView swap

--- app/assets/javascripts/views/lists/show.js
+++ app/assets/javascripts/views/lists/show.js
@@ -1,6 +1,7 @@
 Asana.Views.ListShow = Backbone.CompositeView.extend({
   template: JST['lists/list'],
   events: {
+    'click .editable': showEdit,
   },
 
   className: 'list',
@@ -18,5 +19,16 @@ Asana.Views.ListShow = Backbone.CompositeView.extend({
       var _item = new Asana.Views._Item({ model: item });
       that.addSubview('#list-items', _item.render());
     })
+  },
+
+  showEdit: function(event) {
+    //access this subview -- not sure if this is how
+    oldView = $(event.currentTarget);
+    debugger
+    //create new EditView
+    editView = new Asana.Views._Form({ model = oldView.model });
+    //remove this subview and append EditView
+    this.removeSubview('#list-items', oldView);
+    this.addSubview('#list-items', editView);
   },
 })
