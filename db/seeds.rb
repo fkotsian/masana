3f0545dec9215da6e488e6a1dcbd985b1e8e7a93
App Academy Student <student@appacademy.io>
2014-06-11 15:11:44 -0700

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ guest = User.create([
 
 projects = Project.create([
   # 1
-  { title: 'Get $',
+  { title: 'Work',
     user_id: 1              },
   # 2
   { title: 'Charity',
@@ -26,51 +26,108 @@ projects = Project.create([
 
 lists = List.create([
   # 1
-  { title: 'Berkshire',
-    description: 'Current market cap: 15.35B',
+  { title: 'Learn Angular.js',
+    description: 'The next Backbone',
+    project_id: 1
+  },
+  # 2
+  { title: 'MBTI app?',
+    description: 'Serve info based on Myers-Briggs profiles',
+    project_id: 1
+  },
+  # 3
+  { title: 'Education app review service',
+    description: 'Lots of discussion, but no digital platform',
     project_id: 1
   },
   # 2
   { title: 'Save the Rainforest',
     project_id: 2
   },
-  # 3
+  # 5
+  { title: 'Fix Education',
+    description: 'It starts with helping teachers',
+    project_id: 2
+  },
+  # 6
   { title: 'Find Polar Bears',
     project_id: 3
   },
-  # 4
+  # 
   { title: 'See if Sasquatch is real',
     project_id: 3
   },
-  # 5
+  # 8
   { title: 'Chase a Thunderstorm',
     project_id: 3
   },
-  # 6
-  { title: 'Buy Munger a birthday present',
+  # 9
+  { title: 'Meet Warren Buffett',
     project_id: 3
   }
 ])
 
 list_1_items = List.first.items.create([
-  { title: "Get Money",
-    description: "Got to spend money to make money",
+  { title: "Do Angular tutorial",
+    description: "http://www.toptal.com/angular-js/a-step-by-step-guide-to-your-first-angularjs-app",
     due_date: "1988/11/10",
     rank: 1
   },
-  { title: "Get Paid",
-    description: "Lil B said so",
+  { title: "Build sample app",
+    description: "Perhaps rebuild Masana using Angular",
     due_date: Date.today.to_s,
     rank: 2
   },
-  { title: "Drink Couvoisier with Jay",
-    description: "HOV",
+  { title: "Celebrate",
+    description: "Call everybody",
     due_date: Date.today.to_s,
     rank: 3
   },
-  { title: "Party on a Boat",
+  { title: "Throw Boat Party",
     description: "Need: Nautical-themed Pashmina Afghan",
     due_date: Date.today.to_s,
     rank: 4
+  },
+])
+
+list_7_items = List.find(7).items.create([
+  { title: "Get GPS",
+    rank: 1
+  },
+  { title: "Watch for blurry shadows",
+    rank: 2
+  },
+  { title: "Plant GPS on blurry shadow",
+    rank: 3
+  },
+  { title: "GPS leads me to Sasquatch (or not)",
+    rank: 4
+  },
+  { title: "???",
+    rank: 5
+  },
+  { title: "Profit",
+    rank: 6
+  },
+])
+
+list_8_items = List.find(8).items.create([
+  { title: "Get sweet Jeep",
+    rank: 1
+  },
+  { title: "Move to Philly",
+    rank: 2
+  },
+  { title: "Just hang out",
+    rank: 3
+  },
+  { title: "Found it!",
+    rank: 4
+  },
+  { title: "Give chase to the elements",
+    rank: 5
+  },
+  { title: "Repeat",
+    rank: 6
   },
 ])
